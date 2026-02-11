a0d2e9fbece557a6d06eb4a84d8d110ffcdea1ce
Terry Kim <terrybk@uci.edu>
2026-02-11 07:14:51 -0500

    Made some changes

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -27,6 +27,7 @@ const Parts = ({ part, exercise }) => {
     </li>
   )
 }
+
 const Total = (props) => {
   return (
     <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
