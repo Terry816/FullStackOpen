06fc91d90f2bdb8a283383f9020676dabd5dd287
Terry Kim <terrybk@uci.edu>
2026-02-20 06:49:29 -0500

    Finished part2d exercises

--- part2/exercises/exercises-2.6-2.10/Phonebook/src/App.jsx
+++ part2/exercises/exercises-2.6-2.10/Phonebook/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import personService from './services/person'
 
 const Entry = ({ text, value, handler }) => {
   return (
@@ -19,37 +19,76 @@ const PersonForm = ({ newName, newNumber, namehandler, numberhandler, Addhandler
   )
 }
 
-const Display = ({ view }) => {
+const Display = ({ view, deletehandler }) => {
   return (
     <div>
-      {view.map(p => <p key={p.id}>{p.name} {p.number}</p>)}
+      {view.map(p => (
+        <p key={p.id}>{p.name} {p.number}
+          <button onClick={(e) => deletehandler(e, p.id, p.name)}>delete</button>
+        </p>
+      ))}
     </div>
   )
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newSearch, setNewSearch] = useState('')
 
+  useEffect(() => {
+    console.log("effect")
+    personService.getAll()
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+
   const handleAddName = (event) => {
     event.preventDefault()
-    const hasDupes = persons.find((n) => (n.name === newName))
+    const hasDupes = persons.find((n) => (n.name === newName && n.number === newNumber))
     if (hasDupes !== undefined) {
       alert(`${newName} is already added to phonebook`)
       return;
     }
-    setPersons(prev => prev.concat({ name: newName, number: newNumber, id: String(prev.length + 1) }))
-    setNewName('')
-    setNewNumber('')
+
+    const SameName = persons.find((n) => (n.name === newName))
+    if (SameName !== undefined) {
+      const ok = window.confirm(`${newName} is already added to the phonebook,
+        replace the old number with a new one?`)
+      if (!ok) return
+      const newPerson = { name: SameName.name, number: newNumber }
+      personService.update(SameName.id, newPerson)
+        .then(response => {
+          setPersons(prev => prev.map(node => node.name === SameName.name ? response.data : node))
+          setNewName('')
+          setNewNumber('')
+        })
+    }
+    else {
+      const newPerson = { name: newName, number: newNumber }
+      personService.create(newPerson)
+        .then(response => {
+          setPersons(prev => prev.concat(response.data))
+          setNewName('')
+          setNewNumber('')
+        })
+    }
   }
 
+  const handleDelete = (event, id, name) => {
+    event.preventDefault()
+    const ok = window.confirm("Delete " + name + " ?")
+    if (!ok) return
+    personService.remove(id)
+      .then(() => {
+        setPersons(prev => prev.filter(item => item.id !== id))
+        setNewName('')
+        setNewNumber('')
+      })
+  }
 
   const handleChangeName = (event) => {
     setNewName(event.target.value)
@@ -72,7 +111,7 @@ const App = () => {
       <h2>add a new</h2>
       <PersonForm newName={newName} newNumber={newNumber} namehandler={handleChangeName} numberhandler={handleChangeNumber} Addhandler={handleAddName} />
       <h2>Numbers</h2>
-      <Display view={view} />
+      <Display view={view} deletehandler={handleDelete} />
     </div >
   )
 }
