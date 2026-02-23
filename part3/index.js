f87737384bef23878fc24a920edddaf62c04fad6
Terry Kim <terrybk@uci.edu>
2026-02-22 22:20:28 -0500

    Made changes for Render redeploy

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 app.use(express.json())
+app.use(express.static('dist'))
+const cors = require('cors')
+
+app.use(cors())
 
 let notes = [
   {
@@ -72,7 +76,7 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
